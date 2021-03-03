18af5658d57df55ffe00e8492e3588bb438af89f
Athribis <warren.lorde@gmail.com>
2021-03-03 22:50:52 +0100

    Initialisation d'un objet QFont pour la police du bouton button

--- sources/main.cpp
+++ sources/main.cpp
@@ -3,7 +3,22 @@
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
-    QPushButton button("Hi my name is Kilo");
+    QPushButton button("Bonjour !");
+
+    /*
+     * Création d'une police
+     */
+    QFont maPolice("Courier");
+
+    /*
+     * Création d'un bouton
+     */
+    button.setText("Bonsoir la Vendée !"); //Set un texte
+    button.setToolTip("C'est la Vendée !");
+    button.setFont(maPolice);
+
     button.show();
+    button.setBaseSize(50, 100);
+
     return QApplication::exec();
 }
