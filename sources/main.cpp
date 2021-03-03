40d3a33f6b044c780f9de21e8c24739a030660d2
Athribis <warren.lorde@gmail.com>
2021-03-03 22:23:42 +0100

    Second commit (the good one)

--- sources/main.cpp
+++ sources/main.cpp
@@ -5,5 +5,5 @@ int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
     QPushButton button("Hi my name is Kilo");
     button.show();
-    return a.exec();
+    return QApplication::exec();
 }
