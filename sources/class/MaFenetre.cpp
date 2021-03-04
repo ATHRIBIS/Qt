97f497fb3acc78af852bad68bc69fa463f291ed3
Athribis <warren.lorde@gmail.com>
2021-03-04 16:53:28 +0100

    premier signal : quitter -> quit() -> MaFenetre.cpp

--- sources/class/MaFenetre.cpp
+++ sources/class/MaFenetre.cpp
@@ -5,10 +5,16 @@
 #include "MaFenetre.h"
 
 MaFenetre::MaFenetre() : QWidget() {
-    setFixedSize(300,500);
+    setFixedSize(300, 500);
 
-    m_premierBouton = new QPushButton("Signal n°1", this);
+    m_premierBouton = new QPushButton("Quitter", this);
     m_premierBouton->setFont(QFont("Courier", 14, 75, true));
+
+    /*
+     * Signals
+     * voir p399
+     */
+    QObject::connect(m_premierBouton, SIGNAL(clicked()), qApp, SLOT(quit()));
 }
 
 MaFenetre::~MaFenetre() {
