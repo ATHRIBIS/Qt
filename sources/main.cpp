718293af7b3c96b6b9f3e403f5be2b840ec1817a
Athribis <warren.lorde@gmail.com>
2021-03-04 15:40:09 +0100

    fixup! Fenetre avec un bouton

--- sources/main.cpp
+++ sources/main.cpp
@@ -2,17 +2,19 @@
 #include <QPushButton>
 
 int main(int argc, char *argv[]) {
-    QApplication a(argc, argv);
+    QApplication application(argc, argv);
 
-    QWidget window; //Création d'un widget qui servira de fenêtre
-    window.setFixedSize(300, 150);
+    QWidget fenetre; //Création d'un widget qui servira de fenêtre
+    fenetre.setFixedSize(300, 150);
 
     /*
      * Créatoin du bouton dans la fenêtre
      */
-    QPushButton button("Pimp mon button!", &window); //Création du bouton
+    QPushButton button("Pimp mon button!", &fenetre); //Création du bouton
     button.setFont(QFont("Courier"));
     button.setCursor(QCursor(Qt::BitmapCursor));
 
-    return QApplication::exec();
+    fenetre.show();
+
+    return application.exec();
 }
