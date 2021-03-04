3b8444ffbf491751caf06967230c3570513791b9
Athribis <warren.lorde@gmail.com>
2021-03-04 12:21:15 +0100

    Changement de cursor

--- sources/main.cpp
+++ sources/main.cpp
@@ -11,6 +11,7 @@ int main(int argc, char *argv[]) {
     button.setText("Bonsoir la Vendée !"); //Set un texte
     button.setToolTip("C'est la Vendée !");
     button.setFont(QFont("Courier", 20, QFont::Bold, true)); //On défini directement ici l'objet QFont
+    button.setCursor(Qt::BusyCursor);
 
     button.show();
     button.setBaseSize(300, 100);
