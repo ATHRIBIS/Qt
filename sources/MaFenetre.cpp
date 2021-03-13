41863b2eb2eccc4efd82dc535d2300226fb4b732
athribis <schaeffer.aymeric@gmail.com>
2021-03-13 16:02:06 +0100

    création fenetre dialogue de saisie

--- sources/MaFenetre.cpp
+++ sources/MaFenetre.cpp
@@ -13,16 +13,21 @@ MaFenetre::MaFenetre() : QWidget() {
 }
 
 void MaFenetre::ouvrirDialogue() {
-    //Insérer ici le code d'ouverture d'une boite de dialogue
+//    //Insérer ici le code d'ouverture d'une boite de dialogue
+//
+//    //On créé une varibale answer
+//    //C'est ce qu'on appel une énumartion : en réalité le type n'est pas int mais bien QMessageButton (pour une mielleure lecture)
+//    int answer = QMessageBox::question(this, QString("Couleur de cheveux"), QString("Êtes vous blond ?"), QMessageBox::Yes | QMessageBox::No, QMessageBox::Yes);
+//
+//    //On vérifie la réponse et y attribue une action
+//    if (answer == QMessageBox::Yes) {
+//        QMessageBox::information(this, "Blond", "<a href=\"https://www.youtube.com/watch?v=TBAoMsLODKI\">Vous êtes blond ! blond ! blond !</a>");
+//    } else {
+//        QMessageBox::critical(this, "Pas blond", "Sortez immédiatement de ce programme autorisé uniquement aux personnes blondes !");
+//    }
 
-    //On créé une varibale answer
-    //C'est ce qu'on appel une énumartion : en réalité le type n'est pas int mais bien QMessageButton (pour une mielleure lecture)
-    int answer = QMessageBox::question(this, QString("Couleur de cheveux"), QString("Êtes vous blond ?"), QMessageBox::Yes | QMessageBox::No, QMessageBox::Yes);
-
-    //On vérifie la réponse et y attribue une action
-    if (answer == QMessageBox::Yes) {
-        QMessageBox::information(this, "Blond", "<a href=\"https://www.youtube.com/watch?v=TBAoMsLODKI\">Vous êtes blond ! blond ! blond !</a>");
-    } else {
-        QMessageBox::critical(this, "Pas blond", "Sortez immédiatement de ce programme autorisé uniquement aux personnes blondes !");
-    }
+    /*
+     * On créé une fenetre de dialogue de saisie
+     */
+    QString pseudo = QInputDialog::getText(this, "Pseudo", "Quel est votre pseudo ?");
 }
--- sources/MaFenetre.h
+++ sources/MaFenetre.h
@@ -9,6 +9,15 @@
 #include <QWidget>
 #include <QPushButton>
 #include <QMessageBox> //Necessaire pour afficher des boites de dialogue
+#include <QInputDialog> //Necessaire pour afficher des boites de dialogue de saisie
+/*
+ * Demande :
+ * ::getText -> text
+ * ::getInteger -> entier
+ * ::getDouble -> double
+ * ::getItem -> choisir parmi une liste
+ */
+
 
 class MaFenetre : public QWidget { //Herite notre fenetre de QWidget
     Q_OBJECT
