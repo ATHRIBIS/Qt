35a404dc9c4d1da661243ea4d3c735d800b22d67
athribis <schaeffer.aymeric@gmail.com>
2021-03-13 15:18:45 +0100

    ajout système de traduction de qt

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,11 +1,25 @@
 #include <QApplication>
 #include <QPushButton>
+#include <QTranslator>
+#include <QLocale>
+#include <QLibraryInfo>
 
 #include "MaFenetre.h"
 
 int main(int argc, char *argv[]) {
     QApplication application(argc, argv);
-    MaFenetre fenetre;
-    fenetre.show();
+
+    /*
+     * Traduction des boutons qt
+     * -> des boutons des fenetres "yes" en "oui"
+     */
+    QString locale = QLocale::system().name().section('_',0,0);
+    QTranslator translator;
+    translator.load(QString("qt_") + locale, QLibraryInfo::location(QLibraryInfo::TranslationsPath));
+    QApplication::installTranslator(&translator);
+
+    MaFenetre window;
+    window.show();
+
     return QApplication::exec();
 }
